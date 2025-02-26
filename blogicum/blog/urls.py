71ae89746d0138c65f6d97c644c8fb446d73da7b
Leonid_Kostikov <kostikov.lk@yandex.ru>
2025-02-26 17:50:07 +0600

    blogicum

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 
 from . import views
@@ -9,5 +10,8 @@ urlpatterns = [
     # то вызывается view-функция index() из файла views.py
     path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path('category/<slug:category_slug>/',
+         views.category_posts,
+         name='category_posts'
+         ),
 ]
